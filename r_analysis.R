50b8a80b2ae9e367e2e5a6eb737bcacf6155c015
DinomyteHero <58629291+DinomyteHero@users.noreply.github.com>
2020-07-21 21:12:39 +0200

    Updated the r_analysis

--- r_analysis.R
+++ r_analysis.R
@@ -35,10 +35,22 @@ final_table <- cbind(total_x, total_y, all_subject)
 final_table$activityid <- factor(final_table$activityid, levels = activity_labels[,1], labels = activity_labels[,2])
 final_table$subjectid <- as.factor(final_table$subjectid) 
 
-#Used the aggregate function to apply the mean function to all of the columns except for the subjectid and the activityid
+#Used the aggregate function to find the averages
 
 new_tidy <- aggregate(. ~subjectid + activityid, final_table, mean)
 new_tidy <- new_tidy[order(new_tidy$subjectid, new_tidy$activityid),]
+
+new_tidy2 <- new_tidy[select(new_tidy$subjectid,new_tidy$activityid, contains("mean()" | "std()")),]
+new_tidy2 <- select(new_tidy, matches(mean))
+
+#Need to now create an subset to only display the mean and standard deviations while also displaying the subject id and the name of the activity for easy display
+tidy_names <- colnames(new_tidy)
+
+mean_and_std <- (grepl("activityd" , tidy_names) | grepl("subjectId" , tidy_names) | grepl("mean()" , tidy_names) | grepl("std()" , tidy_names))       
+new_tidy2 <- new_tidy[, mean_and_std == TRUE]
+new_tidy2$subjectid <- new_tidy$subjectid
+new_tidy2$activity <- new_tidy$activityid
+new_tidy2 <- new_tidy2[,c(80:81, 1:79)]
 
 #Created a new textfile for the new table
 
